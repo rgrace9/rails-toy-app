35a2de844d6111a87642dea442997972b37cc8f3
Rebecca Williams <rebecca@informedk12.com>
2021-07-13 18:10:29 -0400

    Finish the toy app.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
 class Micropost < ApplicationRecord
-    validates :content, length: { minimum: 2, maximum: 140 }
+    validates :content, length: { minimum: 2, maximum: 140 }, presence: true
     belongs_to :user
 end
